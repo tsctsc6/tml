df5f2d7ce5d564ca92864bb19b12fbd3e8d014a0
tsctsc6 <tsctsc6@126.com>
2026-06-29 11:15:20 +0800

    fix: do not create Meilisearch index at program start

--- tml/crates/tml_webapi/src/meilisearch.rs
+++ tml/crates/tml_webapi/src/meilisearch.rs
@@ -7,14 +7,5 @@ pub async fn init(app_config: &AppConfig) -> Result<Client, meilisearch_sdk::err
         app_config.meilisearch.host.clone(),
         Some(app_config.meilisearch.api_key.clone()),
     )?;
-    let index = meilisearch_client.index(app_config.meilisearch.index_name.clone());
-    let task = index.set_filterable_attributes(&["artists"]).await?;
-    task.wait_for_completion(&meilisearch_client, None, None)
-        .await?;
-    let task = index
-        .set_searchable_attributes(&["title", "artists", "album_title"])
-        .await?;
-    task.wait_for_completion(&meilisearch_client, None, None)
-        .await?;
     Ok(meilisearch_client)
 }
